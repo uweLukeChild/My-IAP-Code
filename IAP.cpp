999d06d2d257e06c2b7287a18e3aa23c7bc3e0aa
Luke Child <101121708+uweLukeChild@users.noreply.github.com>
2024-08-13 16:25:40 +0100

    Some IAP Code...
    that i did

--- IAP.cpp
+++ IAP.cpp
@@ -1,15 +1,40 @@
-// //// //// //// //// //// //// //// //// //// //// //// //// ////
-//  IAP.cpp
 //
-//  Originally created by Samuel Hunt on 16/07/2018.
-//  Edited by Phill Phelps on 15/12/2023.
+//  IAP.cpp
+//  IAPProjectDevelopmentTest1
+//
+//  Created by Samuel Hunt on 16/07/2018.
+//  Copyright (c) 2018 SJHDevelopment. All rights reserved.
 //
 
 #include "IAP.h"
 #include <iostream>
 
+
 void IAP::run()
+
 {
-    std::cout << "Hello moon!";
-    std::cout << "\n";
+
+  thang.startTimer(200);
+
+  
+
+
+  while(true)
+
+  {
+
+
+    aserveSleep(100);
+
+  }
+
 }
+
+void IAP::callbackNoteReceived  (int note, int velocity, int channel){
+    if(velocity>0)
+    {
+        thang.currentNoteFreq = mtof(note + 2);
+        aserveOscillator(0, mtof(note), 1.0, 0);
+    }
+}
+
--- IAP.h
+++ IAP.h
@@ -1,38 +1,117 @@
-// //// //// //// //// //// //// //// //// //// //// //// //// ////
 //
 //  IAP.h
+//  IAPProjectDevelopmentTest1
 //
-//  Original created by Samuel Hunt on 16/07/2018.
-//  Updated  by Phill Phelps on 15/12/2023.
+//  Created by Samuel Hunt on 16/07/2018.
+//  Copyright (c) 2018 SJHDevelopment. All rights reserved.
 //
 
-#pragma once
+#ifndef __IAPProjectDevelopmentTest1__IAP__
+#define __IAPProjectDevelopmentTest1__IAP__
 
-#include "AserveClient.h"
+#include "AserveComs.h"
 
-class IAP : public AserveClient  {
-public:  
-    //----------------------------------------------------------------------
-    // SHARED VARIABLES (IAP member variables)
-    // ... delete this line and add your own variable declarations below
+//---------------------------------------------------------------------------------
+// USER CREATED CLASS
+class Thingy : public juce::HighResolutionTimer
+
+             , public AserveComs
+
+{
+
+public:
+
+  int counter = 0;
+
+
+
+  Thingy(){};
+
+  ~Thingy(){};
+
+float currentNoteFreq  = 0;
+
+  void hiResTimerCallback() override
+
+  {
+
+    aserveOscillator(0, currentNoteFreq + counter, 0.5, 0);
+      counter++;
+    startTimer( 300 );
+
+  }
+
+
+
+};
+
+
+
+
+
+class IAP : public AserveComs  {
+
+public:
+
   
-    //----------------------------------------------------------------------
-    // FUNCTIONS (IAP class methods)
-    // ... delete this line and add your own function declarations below
+
+    //---------------------------------------------------------------------------------
+
+    // SHARED VARIABLES
+
+    int modwheelValue = 0; // initially set to zero - we check this later
+
   
+
+    //---------------------------------------------------------------------------------
+
+    // FUNCTIONS
+
     void run ();
+
+    float mtof(int note)
+
+    {
+
+      float octave = (note - 69) / 12.0;
+
+      float power = pow(2, octave);
+
+      float freq = 440 * power;
+
   
-    //---------------------------------------------------------------------
+
+
+
+      return freq;
+
+    }
+    
+    //---------------------------------------------------------------------------------
+
     // CALLBACK FUNCTIONS
-    // ... delete this line and enable/disable callback functions below
-  
-    //void callbackNoteReceived  (int note, int velocity, int channel);
+
+
+
+    void callbackNoteReceived  (int note, int velocity, int channel);
+
     //void callbackModWheelMoved (int value);
+
     //void callbackPitchbendWheelMoved (int value);
+
     //void callbackCCValueChanged (int cc, int value);
-    //void callbackMIDIReceived (MIDI message);
-    //void callbackPixelGrid (int x, int y);
+    Thingy thang;
+
+  
+
+
 
 private:
 
+
+
 };
+
+
+ 
+#endif /* defined(__IAPProjectDevelopmentTest1__IAP__) */
